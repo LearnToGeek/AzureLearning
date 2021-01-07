112366fbc116828601edeada56507a5b198db8e3
Ravi Bhalala <rbhalala@visualantidote.com>
2021-01-07 09:08:01 -0500

    script to deploy arm template with multiple resources and WhatIF feature

---
+++ ARM Templates/08-multiple-resource-demo.ps1
@@ -0,0 +1,27 @@
+$rg = '08-multiple-resource-demo'
+
+New-AzResourceGroup `
+    -Name $rg `
+    -Location "Canada Central" `
+    -Tag @{Owner="Ravi";Project="Learning"}
+
+$timestamp=get-date -f MM-dd-yyyy_HH_mm_ss
+$deploymentName="deployment_"+"$today"
+$templateFile = ".\08-multiple-resource-demo.json"
+$parameterFile = ".\08-multiple-resource-demo.parameters.json"
+
+#With WhatIF option to validate template
+New-AzResourceGroupDeployment `
+    -Name $deploymentName `
+    -ResourceGroupName $rg `
+    -WhatIf `
+    -TemplateFile $templateFile `
+    -TemplateParameterFile $parameterFile
+
+#Deploy template
+New-AzResourceGroupDeployment `
+    -Name $deploymentName `
+    -ResourceGroupName $rg `
+    -TemplateFile $templateFile `
+    -TemplateParameterFile $parameterFile
+

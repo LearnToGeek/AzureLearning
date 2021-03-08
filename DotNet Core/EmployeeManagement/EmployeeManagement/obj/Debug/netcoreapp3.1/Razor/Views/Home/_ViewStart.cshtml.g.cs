19a7ac11e0da896c9ca4a360dd57d82536bdabc2
Ravi Bhalala <rbhalala@visualantidote.com>
2021-03-08 08:39:30 -0500

    Helper tag with bootstrap styling

--- DotNet Core/EmployeeManagement/EmployeeManagement/obj/Debug/netcoreapp3.1/Razor/Views/Home/_ViewStart.cshtml.g.cs
+++ DotNet Core/EmployeeManagement/EmployeeManagement/obj/Debug/netcoreapp3.1/Razor/Views/Home/_ViewStart.cshtml.g.cs
@@ -13,22 +13,21 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
 #nullable restore
-#line 1 "C:\Azure\AzureLearning\DotNet Core\EmployeeManagement\EmployeeManagement\Views\Home\_ViewImports.cshtml"
+#line 1 "C:\Azure\AzureLearning\DotNet Core\EmployeeManagement\EmployeeManagement\Views\_ViewImports.cshtml"
 using EmployeeManagement.ViewModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Azure\AzureLearning\DotNet Core\EmployeeManagement\EmployeeManagement\Views\Home\_ViewImports.cshtml"
+#line 2 "C:\Azure\AzureLearning\DotNet Core\EmployeeManagement\EmployeeManagement\Views\_ViewImports.cshtml"
 using EmployeeManagement.Models;
 
 #line default
 #line hidden
 #nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"53b4c58ae92a9d081aeb602325505570af15f7e0", @"/Views/Home/_ViewStart.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"57218c316b6921e2cd61027a2387edc31a2d9471", @"/Views/_ViewImports.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7b1b099d611b92c7cada047478ff9597b4580632", @"/Views/Home/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"291b1e14a42ee6495c0f5bac2d13c515e7927c54", @"/Views/_ViewImports.cshtml")]
     public class Views_Home__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
--- DotNet Core/EmployeeManagement/EmployeeManagement/obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
+++ DotNet Core/EmployeeManagement/EmployeeManagement/obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
@@ -12,8 +12,22 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Azure\AzureLearning\DotNet Core\EmployeeManagement\EmployeeManagement\Views\_ViewImports.cshtml"
+using EmployeeManagement.ViewModels;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Azure\AzureLearning\DotNet Core\EmployeeManagement\EmployeeManagement\Views\_ViewImports.cshtml"
+using EmployeeManagement.Models;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7091c65830b0329e613be026ede8a57552863778", @"/Views/_ViewStart.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"57218c316b6921e2cd61027a2387edc31a2d9471", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"291b1e14a42ee6495c0f5bac2d13c515e7927c54", @"/Views/_ViewImports.cshtml")]
     public class Views__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
